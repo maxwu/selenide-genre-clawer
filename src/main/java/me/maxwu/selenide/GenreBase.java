5a0dcd3cd21ee44bedaa5543c4a36fae9c2b83d2
maxwu <maxwunj@gmail.com>
2017-03-18 01:44:46 +1300

    add JetBrains annotation

--- src/main/java/me/maxwu/selenide/GenreBase.java
+++ src/main/java/me/maxwu/selenide/GenreBase.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Configuration.screenshots;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Configuration.baseUrl;
@@ -38,6 +41,23 @@ public class AppBase {
        setDriver();
        baseUrl = "http://google.com";
        return open("/", GooglePage.class);
+    }
+
+    /**
+     * The logic to fetch genre is:
+     *  - Try "$song genre";
+     *  - If NA, try "$song song genre", otherwise return genre list
+     *  - Return genre list, for failed trial, return {"NA"}.
+     * @param song
+     * @return
+     */
+    public List<String> getSongGenres(String song){
+        List<String> genres = new ArrayList<>(4);
+        genres = onGooglePage().getSearchFor(GooglePage.keywordSongGenre(song)).getGenres();
+        if (genres.get(0).equals("NA")){
+            genres = onGooglePage().getSearchFor(GooglePage.keywordGenre(song)).getGenres();
+        }
+        return genres;
     }
 
     public static void quitDriver(){
